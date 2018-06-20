3192fecff01aab37db6bbc7e7e621b9aedd1a77d
Alan Knight <alanknight@google.com>
2018-06-20 13:29:28 -0700

    Working for simple Dart expressions

--- front_end/sdk/DartSupport.js
+++ front_end/sdk/DartSupport.js
@@ -17,15 +17,15 @@ if (typeof $d == 'undefined') {
   window.$d = window.$dartEvaluateExpression;
 }
 
-var lookupInJSScope = `(function(name) {
+var lookupInJSScope = `function lookupInJsScope(name) {
   try {
     if (name != window[name] || !(name in window)) {
       return name;
     }
   } catch(e) {}
-})`;
+}`;
 
-var lookupInThis = `(function(__this, name) {
+var lookupInThis = `function lookupInThis(__this, name) {
   var found = false;
   let type = dart.getReifiedType(__this) == "NativeJavaScriptObject"
       ? null : dart.getType(__this);
@@ -36,7 +36,7 @@ var lookupInThis = `(function(__this, name) {
     }
   });
   if (found) return name;
-})`;
+};`;
 
 window.$dartExpressionFor = function(executionContext, dartExpression) {
   var components = dartExpression.split('.');
@@ -44,27 +44,38 @@ window.$dartExpressionFor = function(executionContext, dartExpression) {
   // A crude check if all of our components look like valid Dart
   // identifiers.  If any of them fail, just return the original
   // expression and let it be evaluated as Javascript;
-
   var looksLikeIdentifier = /^[_\$a-zA-Z0-9]*$/g;
   for (let component of components) {
-    if (!looksLikeIdentifier.exec(component)) return dartExpression;
+    if (!component.match(looksLikeIdentifier)) {
+      return `console.log("%c(Cannot evaluate as a Dart expression, using JS eval)",
+          "background-color: hsl(50, 100%, 95%)")
+      ${dartExpression};`;
+    }
   }
   var receiver = components[0];
   components.shift();
-  var expression = '(function(__this) {\n';
-  expression += "debugger;";
-  expression += "const dart = dart_library.import('dart_sdk').dart;\n";
-  name = receiver;
-  expression += 'var jsScopeName = ' + lookupInJSScope + '("' + receiver + '");\n';
-  expression += 'var thisScopeName = ' + lookupInThis + '(__this,"' + receiver + '");\n';
-  expression += 'var receiverObject;\n';
-  expression += "if (thisScopeName) receiverObject = dart.dloadRepl(__this, thisScopeName);\n";
-  expression += "var result = receiverObject || eval(jsScopeName || receiver);\n";
+  var expression = `
+(function(__this) {
+  // TODO: These should probably be helper functions in the DDC runtime.
+${lookupInJSScope}
+${lookupInThis}
+
+  const dart = dart_library.import('dart_sdk').dart;
+  var name = "${receiver}";
+  var jsScopeName = lookupInJsScope(name);
+  var thisScopeName = lookupInThis(__this,name);
+  var receiverObject;
+  if (name == "this") {
+    receiverObject = __this;
+  } else if (thisScopeName) {
+    receiverObject = dart.dloadRepl(__this, thisScopeName);
+  }
+  var result = receiverObject || eval(jsScopeName || name);`
+
   for (let getter of components) {
-    expression += 'result = dart.dloadRepl(result, "' + getter + '");';;
+    expression += `result = dart.dloadRepl(result, "${getter}");\n`
   }
   expression += 'return result;})(this)';
-//  console.log("Converted Dart expression to:\n" + expression);
   return expression;
 }
 
